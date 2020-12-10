3fac09e65aca28ed754cd1440969cd3aeddebcd4
Anatoly Kosharovsky <kosharovsky@hotmail.com>
2020-12-10 14:16:23 -0500

    First Commit

---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (101, 'Anatoly', 'Java');
+insert into alien values (102, 'Navin', 'Android');
+insert into alien values (103, 'Kiran', 'Python');
+insert into alien values (104, 'Komal', 'Java');
+insert into alien values (105, 'Rafiq', 'Android');
